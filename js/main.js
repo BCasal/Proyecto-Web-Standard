4666f704ab55d3e57dd13127181966bbdf26e98d
BCasal <bcasal@bcasal.es>
2016-01-28 15:37:14 +0100

    Invertir condición if test cssvhunit

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 
 
 (function() {
-	if (Modernizr.cssvhunit) {
-		alert('Este navegador «Si» soporta VH');
-	} else {
+	if (!Modernizr.cssvhunit) {
 		alert('Este navegador «NO» soporta VH');
+	} else {
+		alert('Este navegador «SI» soporta VH');
 	}
 }).call(this);
